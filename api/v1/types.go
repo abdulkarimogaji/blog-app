7df872ab813372737ef9d45878308ef2270bc700
Abdulkarim Ogaji <abdulkarimogaji001@gmail.com>
2023-03-15 04:23:37 +0100

    feat: request body validation

---
+++ api/v1/types.go
@@ -0,0 +1,13 @@
+package v1
+
+import "time"
+
+type user struct {
+	Id        int       `json:"id"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password,omitempty"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
---
+++ api/v1/validation.go
@@ -0,0 +1,58 @@
+package v1
+
+import (
+	"errors"
+	"io"
+	"net/http"
+	"regexp"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+)
+
+type errorMsg struct {
+	Field   string `json:"field"`
+	Message string `json:"message"`
+}
+
+var matchFirstCap = regexp.MustCompile("(.)([A-Z][a-z]+)")
+var matchAllCap = regexp.MustCompile("([a-z0-9])([A-Z])")
+
+func getErrorMsg(fe validator.FieldError) string {
+	switch fe.Tag() {
+	case "required":
+		return "This field is required"
+	case "lte":
+		return "Should be less than " + fe.Param()
+	case "gte":
+		return "Should be greater than " + fe.Param()
+	case "min":
+		return toSnakeCase(fe.Field()) + " should be at least " + fe.Param() + " characters"
+	}
+	return "Unknown error"
+}
+
+func validationResponse(err error, c *gin.Context) {
+
+	if err == io.EOF {
+		c.JSON(http.StatusBadRequest, gin.H{"success": false, "message": "empty body"})
+		return
+	}
+
+	var ve validator.ValidationErrors
+	if errors.As(err, &ve) {
+		out := make([]errorMsg, len(ve))
+		for i, fe := range ve {
+			out[i] = errorMsg{toSnakeCase(fe.Field()), getErrorMsg(fe)}
+		}
+		c.JSON(http.StatusBadRequest, gin.H{"success": false, "message": "validation error", "errors": out})
+		return
+	}
+}
+
+func toSnakeCase(str string) string {
+	snake := matchFirstCap.ReplaceAllString(str, "${1}_${2}")
+	snake = matchAllCap.ReplaceAllString(snake, "${1}_${2}")
+	return strings.ToLower(snake)
+}
